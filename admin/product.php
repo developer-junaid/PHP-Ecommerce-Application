f86e9dcbf463c564cf9d1143884c7f30de14f274
Junaid <qjunaid623@gmail.com>
2020-09-14 23:11:25 +0500

    Added Contact Us and Product Page in Admin Panel
    Continue from Manage Product page

--- admin/product.php
+++ admin/product.php
@@ -1,5 +1,40 @@
 <?php
    require('header.inc.php');
+   
+   // Get Status data from categories
+   if (isset ($_GET['type']) && $_GET['type']!='' ){
+       $type = get_safe_value($con, $_GET['type']);
+       
+       // If Status is provided
+       if ($type == 'status'){
+        $id = get_safe_value($con, $_GET['id']);
+        $operation = get_safe_value($con, $_GET['operation']);
+
+        // If it is active
+        if ($operation == 'active'){
+            $status = '1';
+        }else{
+            $status = '0';
+        }
+
+
+        $update_status_sql = "update product set status='$status' where id='$id' ";
+        mysqli_query($con, $update_status_sql);
+       }
+
+       // If Delete is provided
+       if ($type == 'delete'){
+         $id = get_safe_value($con, $_GET['id']);
+         $delete_sql = "delete from product where id='$id' ";
+         mysqli_query($con, $delete_sql);
+        }
+   }
+   
+   // FETCH Categories Data from Database
+   $query = "select product.*, categories.categories from product, categories where product.categories_id=categories.id order by product.id desc";
+   $result= mysqli_query($con, $query);
+
+   
 ?>
 
    <div class="content pb-0">
@@ -8,7 +43,8 @@
                   <div class="col-xl-12">
                      <div class="card">
                         <div class="card-body">
-                           <h4 class="box-title">Orders </h4>
+                           <h4 class="box-title">Products </h4>
+                           <h4 class="box-title" style='font-size:15px;'> <a style='color: black; text-decoration: underline;' href="manage_product.php">Add Product</a>  </h4>
                         </div>
                         <div class="card-body--">
                            <div class="table-stats order-table ov-h">
@@ -16,90 +52,43 @@
                                  <thead>
                                     <tr>
                                        <th class="serial">#</th>
-                                       <th class="avatar">Avatar</th>
                                        <th>ID</th>
+                                       <th>Categories</th>
                                        <th>Name</th>
-                                       <th>Product</th>
+                                       <th>Image</th>
+                                       <th>Price</th>
                                        <th>Quantity</th>
-                                       <th>Status</th>
+                                       <th></th>
+
                                     </tr>
                                  </thead>
                                  <tbody>
-                                    <tr>
-                                       <td class="serial">1.</td>
-                                       <td class="avatar">
-                                          <div class="round-img">
-                                             <a href="#"><img class="rounded-circle" src="images/avatar/1.jpg" alt=""></a>
-                                          </div>
-                                       </td>
-                                       <td> #5469 </td>
-                                       <td> <span class="name">Louis Stanley</span> </td>
-                                       <td> <span class="product">iMax</span> </td>
-                                       <td><span class="count">231</span></td>
-                                       <td>
-                                          <span class="badge badge-complete">Complete</span>
-                                       </td>
-                                    </tr>
-                                    <tr>
-                                       <td class="serial">2.</td>
-                                       <td class="avatar">
-                                          <div class="round-img">
-                                             <a href="#"><img class="rounded-circle" src="images/avatar/2.jpg" alt=""></a>
-                                          </div>
-                                       </td>
-                                       <td> #5468 </td>
-                                       <td> <span class="name">Gregory Dixon</span> </td>
-                                       <td> <span class="product">iPad</span> </td>
-                                       <td><span class="count">250</span></td>
-                                       <td>
-                                          <span class="badge badge-complete">Complete</span>
-                                       </td>
-                                    </tr>
-                                    <tr>
-                                       <td class="serial">3.</td>
-                                       <td class="avatar">
-                                          <div class="round-img">
-                                             <a href="#"><img class="rounded-circle" src="images/avatar/3.jpg" alt=""></a>
-                                          </div>
-                                       </td>
-                                       <td> #5467 </td>
-                                       <td> <span class="name">Catherine Dixon</span> </td>
-                                       <td> <span class="product">SSD</span> </td>
-                                       <td><span class="count">250</span></td>
-                                       <td>
-                                          <span class="badge badge-complete">Complete</span>
-                                       </td>
-                                    </tr>
-                                    <tr>
-                                       <td class="serial">4.</td>
-                                       <td class="avatar">
-                                          <div class="round-img">
-                                             <a href="#"><img class="rounded-circle" src="images/avatar/4.jpg" alt=""></a>
-                                          </div>
-                                       </td>
-                                       <td> #5466 </td>
-                                       <td> <span class="name">Mary Silva</span> </td>
-                                       <td> <span class="product">Magic Mouse</span> </td>
-                                       <td><span class="count">250</span></td>
-                                       <td>
-                                          <span class="badge badge-pending">Pending</span>
-                                       </td>
-                                    </tr>
-                                    <tr class=" pb-0">
-                                       <td class="serial">5.</td>
-                                       <td class="avatar pb-0">
-                                          <div class="round-img">
-                                             <a href="#"><img class="rounded-circle" src="images/avatar/6.jpg" alt=""></a>
-                                          </div>
-                                       </td>
-                                       <td> #5465 </td>
-                                       <td> <span class="name">Johnny Stephens</span> </td>
-                                       <td> <span class="product">Monitor</span> </td>
-                                       <td><span class="count">250</span></td>
-                                       <td>
-                                          <span class="badge badge-complete">Complete</span>
-                                       </td>
-                                    </tr>
+                                     <?php 
+                                     $i = 1;
+                                     while($row = mysqli_fetch_assoc($result)){ ?>
+                                        <tr>
+                                            <td class="serial"><?php echo $i ?></td>
+                                            <td><?php echo $row['id'] ?></td>
+                                            <td><?php echo $row['categories'] ?></td>
+                                            <td><?php echo $row['name'] ?></td>
+                                            <td><?php echo $row['image'] ?></td>
+                                            <td><?php echo $row['price'] ?></td>
+                                            <td><?php echo $row['qty'] ?></td>
+
+                                            <td><?php 
+                                            if ($row['status'] == 1){
+                                                echo "<span class='badge badge-complete'> <a style='color: white;' href='?type=status&operation=deactive&id=".$row['id']."'>Active</a></span>&nbsp;";
+                                            }else{
+                                                echo "<span class='badge badge-pending'> <a style='color: white;' href='?type=status&operation=active&id=".$row['id']."'>Deactive</a></span>&nbsp;";
+                                            }
+                                            echo "<span class='badge badge-edit'> <a style='color: white;' href='manage_categories.php?id=".$row['id']."'>Edit</a></span>&nbsp;";
+                                            echo "<span class='badge badge-delete'> <a style='color: white;' href='?type=delete&id=".$row['id']."'>Delete</a></span>";
+
+                                        ?></td>
+                                        </tr>
+                                    <?php 
+                                    $i++;
+                                }  ?>
                                  </tbody>
                               </table>
                            </div>
